9b6e3196403b3739f93465b2ef707da6cd1648a7
Roel Henckaerts <roelhenckaerts@gmail.com>
2020-04-03 16:33:03 +0200

    Update autotune to improve efficiency

--- R/autotune.R
+++ R/autotune.R
@@ -4,40 +4,53 @@
 #' feature selection. Lambda directly influences the granularity of the
 #' segmentation, with low/high values resulting in a fine/coarse segmentation.
 #'
-#' @param fx_vars A list of data frames containing the feature effects. These
-#'   can be obtained by applying \code{\link{insights}} on your model fit.
+#' @param mfit Fitted model object (e.g., a "gbm" or "randomForest" object).
 #' @param data Data frame containing the original training data.
-#' @param target A string specifying the target (or response) variable to model.
-#' @param lambdas A numeric vector with the possible lambda values to explore.
-#'   The search grid is created automatically via \code{\link{lambda_grid}}.
-#'   This grid contains only those values of lambda that result in a unique
-#'   grouping of the full set of features. If both main and interaction effects
-#'   are present in \code{fx_vars}, a seperate grid is generated and all the
-#'   pairwise combinations are evaluated in a cartesian grid search.
-#' @param nfolds An integer for the number of folds in K-fold cross-validation.
-#' @param strat_vars A character (vector) specifying the feature(s) to use for
+#' @param vars Character vector specifying the features in \code{data} to use.
+#' @param target String specifying the target (or response) variable to model.
+#' @param hcut Numeric in the range \[0,1\] specifying the cut-off value for the
+#'   normalized cumulative H-statistic over all two-way interactions. See the
+#'   documentation of \code{\link{insights}} for the details on this argument.
+#'   Setting \code{hcut = -1} will only consider main effects in the tuning.
+#' @param pred_fun Optional prediction function to calculate feature effects
+#'   for the model in \code{mfit}. Requires two arguments: \code{object} and
+#'   \code{newdata}. See \code{\link[pdp:partial]{pdp::partial}} for the details
+#'   (\url{https://bgreenwell.github.io/pdp/articles/pdp-extending.html}).
+#' @param lambdas Numeric vector with the possible lambda values to explore.
+#'   The search grid is created automatically via \code{\link{lambda_grid}}
+#'   such that it contains only those values of lambda that result in a unique
+#'   grouping of the full set of features. A seperate grid is generated for
+#'   main and interaction effects, due to the scale difference in both types.
+#' @param nfolds Integer for the number of folds in K-fold cross-validation.
+#' @param strat_vars Character (vector) specifying the feature(s) to use for
 #'   stratified sampling. The default NULL implies no stratification is applied.
-#' @param glm_par A named list, constructed via \code{\link{alist}}, containing
+#' @param glm_par Named list, constructed via \code{\link{alist}}, containing
 #'   arguments to be passed on to \code{\link[stats]{glm}}. Examples are:
 #'   \code{family}, \code{weights} or \code{offset}. Note that \code{formula}
 #'   will be ignored as the GLM formula is determined by the specified
 #'   \code{target} and the automatic feature selection in the tuning process.
-#' @param err_fun An error function to calculate the prediction errors on the
+#' @param err_fun Error function to calculate the prediction errors on the
 #'   validation folds. This must be an R function which outputs a single number
 #'   and takes two vectors \code{y_pred} and \code{y_true} as input for the
 #'   predicted and true target values respectively. An additional input vector
 #'   \code{w_case} is allowed to use case weights in the error function. The
 #'   weights are determined automatically based on the \code{weights} field
 #'   supplied to \code{glm_par}. Examples already included in the package:
-#'   \describe{ \item{mse}{mean squared error loss function.}
+#'   \describe{ \item{mse}{mean squared error loss function (default).}
 #'   \item{wgt_mse}{weighted mean squared error loss function.}
 #'   \item{poi_dev}{Poisson deviance loss function.} }
-#' @param ncores An integer specifying the number of cores to use. The default
+#' @param ncores Integer specifying the number of cores to use. The default
 #'   \code{ncores = -1} uses all the available physical cores (not threads), as
 #'   determined by \code{parallel::detectCores(logical = 'FALSE')}.
-#' @return A tidy data frame (i.e., a "tibble" object) with the cross-validation
-#'   results. The column \code{cv_err} contains the cross-validated error, while
-#'   the columns \code{1:nfolds} contain the error on the validation folds.
+#' @return List with four elements: \describe{ \item{slct_feat}{named vector
+#'   containing the selected features (names) and the optimal number of groups
+#'   for each feature (values).} \item{best_surr}{the optimal GLM surrogate,
+#'   which is fit to all observations in \code{data}. The segmented data can be
+#'   obtained via the \code{$data} attribute of the GLM fit.} \item{tune_main}{
+#'   the cross-validation results for the main effects as a tidy data frame.
+#'   The column \code{cv_err} contains the cross-validated error, while
+#'   the columns \code{1:nfolds} contain the error on the validation folds.}
+#'   \item{tune_intr}{cross-validation results for the interaction effects.}}
 #' @examples
 #' \dontrun{
 #' data('mtpl_be')
@@ -51,14 +64,12 @@
 #'                     interaction.depth = 3,
 #'                     shrinkage = 0.1)
 #' gbm_fun <- function(object, newdata) mean(predict(object, newdata, n.trees = object$n.trees, type = 'response'))
-#' gbm_fit %>% insights(vars = c('ageph', 'bm', 'coverage', 'fuel'),
-#'                      data = mtpl_be,
-#'                      interactions = 'auto',
-#'                      hcut = 0.7,
-#'                      pred_fun = gbm_fun) %>%
-#'             autotune(data = mtpl_be,
+#' gbm_fit %>% autotune(data = mtpl_be,
+#'                      vars = c('ageph', 'bm', 'coverage', 'fuel', 'sex', 'fleet', 'use'),
 #'                      target = 'nclaims',
-#'                      lambdas = as.vector(outer(seq(1, 10, 1), 10^(-3:-1))),
+#'                      hcut = 0.75,
+#'                      pred_fun = gbm_fun,
+#'                      lambdas = as.vector(outer(seq(1, 10, 1), 10^(-6:-2))),
 #'                      nfolds = 5,
 #'                      strat_vars = c('nclaims', 'expo'),
 #'                      glm_par = alist(family = poisson(link = 'log'),
@@ -67,9 +78,13 @@
 #'                      ncores = -1)
 #' }
 #' @export
-autotune <- function(fx_vars, data, target, lambdas = as.vector(outer(seq(1, 10, 0.1), 10^(-7:3))), nfolds = 5, strat_vars = NULL, glm_par = alist(), err_fun = mse, ncores = -1) {
+autotune <- function(mfit, data, vars, target, hcut = 0.75, pred_fun = NULL, lambdas = as.vector(outer(seq(1, 10, 0.1), 10^(-7:3))), nfolds = 5, strat_vars = NULL, glm_par = alist(), err_fun = mse, ncores = -1) {
 
+  if (sum(grepl('_', vars)) > 0) stop('No underscores allowed in the variable names, these are interpreted as interactions in maidrr.')
+  if (! all(vars %in% names(data))) stop('All the variables needs to be present in the data.')
   if (! target %in% names(data)) stop('The target variable needs to be present in the data.')
+
+  if (! ((hcut == -1) | (0 <= hcut & hcut <= 1))) stop('Invalid value specified for hcut, must equal -1 or lie within the range [0, 1].')
 
   if (nfolds <= 1) stop('At least two folds are needed to perform K-fold cross-validation.')
 
@@ -87,57 +102,132 @@ autotune <- function(fx_vars, data, target, lambdas = as.vector(outer(seq(1, 10,
   if (ncores < -1 | ncores == 0) stop('The number of cores must be strictly positive, or equal to -1 for all available cores.')
   if (ncores > parallel::detectCores()) warning('The asked number of cores is larger than parallel::detectCores, so there might be trouble ahead.')
 
-  # Create the grid of lambda values
-  lmbd_grid <- maidrr::lambda_grid(fx_vars = fx_vars, lambda_range = lambdas)
+  # Function to calculate validation errors
+  Kfold_cross_valid <- function(data, target, nfolds, glm_par, err_fun, fixed_terms = NULL){
+    val_err <- rep(NA, nfolds)
+    for (f in seq_len(nfolds)) {
+      # Get training fold
+      trn_data <- dplyr::filter(data, fold != f)
+      # Feature selection (only keep those with >1 group) and determine the GLM formula
+      slct_feat <- names(which(unlist(lapply(trn_data[, grepl('_$', names(trn_data))], function(x) length(unique(x)) > 1))))
+      glm_par[['formula']] <- as.formula(paste(target, '~', paste(c(1, fixed_terms, slct_feat), collapse = ' + ')))
+      # Fit surrogate GLM on the training fold
+      glm_fit <- maidrr::surrogate(trn_data, glm_par)
+      # Get validation fold and calculate the prediction error
+      val_data <- maidrr::rm_lvls(glm_fit, dplyr::filter(data, fold == f))
+      y_pred <- suppressWarnings(predict(glm_fit, newdata = val_data, type = 'response'))
+      y_true <- dplyr::pull(val_data, target)
+      w_case <- val_data[[deparse(glm_fit$call$weights)]]
+      val_err[f] <- ifelse(length(glm_fit$coefficients) > glm_fit$rank, Inf,
+                           do.call(err_fun, setNames(lapply(names(formals(err_fun)), function(x) eval(as.name(x))), names(formals(err_fun)))))
+    }
+    return(val_err)
+  }
 
   # Create the fold indicator via (stratified) sampling
   data <- data %>% dplyr::sample_n(size = nrow(.), replace = FALSE) %>%
-                   dplyr::arrange(!!! rlang::syms(strat_vars)) %>%
-                   dplyr::mutate(fold = rep_len(seq_len(nfolds), nrow(.)))
+    dplyr::arrange(!!! rlang::syms(strat_vars)) %>%
+    dplyr::mutate(fold = rep_len(seq_len(nfolds), nrow(.)))
 
+  # Generate the feature effects and the lambda grid for main effects
+  fx_main <- maidrr::insights(mfit = mfit,
+                              vars = vars,
+                              data = data,
+                              interactions = 'user',
+                              pred_fun = pred_fun)
+  lmbd_main <- maidrr::lambda_grid(fx_vars = fx_main, lambda_range = lambdas)
+
+  # Set up a cluster and iterate over the lambda grid
   switch(as.character(ncores),
          '-1' = doParallel::registerDoParallel(parallel::detectCores(logical = 'FALSE')),
          '1' = foreach::registerDoSEQ(),
          doParallel::registerDoParallel(ncores))
-
-  # Iterate over the lambda grid
-  out <- foreach::foreach (l = seq_len(nrow(lmbd_grid)), .combine = rbind) %dopar% {
-
+  out_main <- foreach::foreach (l = seq_len(nrow(lmbd_main)), .combine = rbind) %dopar% {
     # Segmentation for current lambda values
-    data_segm <- data
-    for (i in seq_len(length(fx_vars))) {
-      fx_var <- fx_vars[[i]]
-      var <- fx_var %>% comment
-      fx_grp <- fx_var %>% maidrr::group_pd(ngroups = lmbd_grid[[l, var]])
-
-      data_segm <- data_segm %>% dplyr::left_join(fx_grp[c(paste0('x', if (grepl('_', var)) 1:2), 'xgrp')],
-                                                  by = setNames(paste0('x', if (grepl('_', var)) 1:2), unlist(strsplit(var, '_')))) %>%
-        dplyr::mutate(xgrp = relevel(as.factor(xgrp), ref =  (fx_grp %>% dplyr::arrange(-wgrp) %>% dplyr::pull(xgrp))[1])) %>%
-        dplyr::rename(!!paste0(var, '_') := xgrp)
-    }
-
+    data_segm <- maidrr::segmentation(fx_vars = fx_main, data = data, type = 'ngroups',
+                                      values =  unlist(dplyr::select(dplyr::slice(lmbd_main, l), vars), use.names = TRUE))
     # Calculate the validation errors
-    val_err <- rep(NA, nfolds)
-    for (f in seq_len(nfolds)) {
-      # Get training fold
-      trn_data <- dplyr::filter(data_segm, fold != f)
-      # Feature selection (only keep those with >1 group) and determine the GLM formula
-      slct_feat <- names(which(unlist(lapply(trn_data[, grepl('_$', names(trn_data))], function(x) length(unique(x)) > 1))))
-      glm_par[['formula']] <- as.formula(paste(target, '~', paste(c(1, slct_feat), collapse = ' + ')))
-      # Fit surrogate GLM on the training fold
-      glm_fit <- maidrr::surrogate(trn_data, glm_par)
-      # Get validation fold and calculate the prediction error
-      val_data <- maidrr::rm_lvls(glm_fit, dplyr::filter(data_segm, fold == f))
-      y_pred <- predict(glm_fit, newdata = val_data, type = 'response')
-      y_true <- dplyr::pull(val_data, target)
-      w_case <- val_data[[deparse(glm_fit$call$weights)]]
-      val_err[f] <- do.call(err_fun, setNames(lapply(names(formals(err_fun)), function(x) eval(as.name(x))), names(formals(err_fun))))
-    }
-    val_err
+    Kfold_cross_valid(data = data_segm, target = target, nfolds = nfolds, glm_par = glm_par, err_fun = err_fun)
   }
   doParallel::stopImplicitCluster()
 
-  dplyr::bind_cols(lmbd_grid, out %>% as.data.frame %>% setNames(seq_len(nfolds))) %>%
-    dplyr::mutate(cv_err = rowMeans(dplyr::select(., as.character(seq_len(nfolds))), na.rm = TRUE)) %>%
-    dplyr::arrange(cv_err)
+  # Get the cross-validation error
+  out_main <- dplyr::bind_cols(lmbd_main, out_main %>% as.data.frame %>% setNames(seq_len(nfolds))) %>%
+    dplyr::mutate(cv_err = rowMeans(dplyr::select(., as.character(seq_len(nfolds))), na.rm = TRUE)) %>% dplyr::arrange(cv_err)
+
+  # Get the selected features
+  slct_main <- out_main %>% dplyr::slice(1) %>% dplyr::select(!!!rlang::syms(vars)) %>%
+    dplyr::select_if(~. > 1) %>% unlist(., use.names = TRUE)
+  if (length(slct_main) == 0) stop('Not a single feature was selected, please try again with lower values for lambda.')
+
+  # Segment the data based on the selected features and optimal number of groups
+  data_main <- maidrr::segmentation(fx_vars = fx_main[names(slct_main)], data = data, type = 'ngroups', values = slct_main)
+
+
+  # Return these results if no interactions are asked for
+  if (hcut == -1) {
+    # Fit the optimal surrogate GLM
+    glm_par[['formula']] <- as.formula(paste(target, '~', paste(c(1, paste0(names(slct_main), '_')), collapse = ' + ')))
+    opt_surro <- maidrr::surrogate(data = data_main, par_list = glm_par)
+
+    # Combine results in a list and return
+    output <- list('slct_feat' = slct_main,
+                   'best_surr' = opt_surro,
+                   'tune_main' = out_main,
+                   'tune_intr' = NULL)
+    return(output)
+  }
+
+
+  # Generate the feature effects and the lambda grid for interactions
+  fx_intr <- maidrr::insights(mfit = mfit,
+                              vars = names(slct_main),
+                              data = data,
+                              interactions = 'auto',
+                              hcut = hcut,
+                              pred_fun = pred_fun,
+                              fx_in = fx_main)
+  fx_intr <- fx_intr[grepl('_', names(fx_intr))]
+  vars_intr <- names(fx_intr)
+  lmbd_intr <- maidrr::lambda_grid(fx_vars = fx_intr, lambda_range = lambdas)
+
+  # Set up a cluster and iterate over the lambda grid
+  switch(as.character(ncores),
+         '-1' = doParallel::registerDoParallel(parallel::detectCores(logical = 'FALSE')),
+         '1' = foreach::registerDoSEQ(),
+         doParallel::registerDoParallel(ncores))
+  out_intr <- foreach::foreach (l = seq_len(nrow(lmbd_intr)), .combine = rbind) %dopar% {
+    # Segmentation for current lambda values
+    data_segm <- maidrr::segmentation(fx_vars = fx_intr, data = data_main, type = 'ngroups',
+                                      values =  unlist(dplyr::select(dplyr::slice(lmbd_intr, l), vars_intr), use.names = TRUE))
+    # Calculate the validation errors
+    Kfold_cross_valid(data = data_segm, target = target, nfolds = nfolds, glm_par = glm_par, err_fun = err_fun, fixed_terms = paste0(names(slct_main), '_'))
+  }
+  doParallel::stopImplicitCluster()
+
+  # Get the cross-validation error
+  out_intr <- dplyr::bind_cols(lmbd_intr, out_intr %>% as.data.frame %>% setNames(seq_len(nfolds))) %>%
+    dplyr::mutate(cv_err = rowMeans(dplyr::select(., as.character(seq_len(nfolds))), na.rm = TRUE)) %>% dplyr::arrange(cv_err)
+  if (all(out_intr$cv_err == Inf)) warning('All interaction combinations lead to rank-deficient GLM fits, try adding larger values for lambda to the grid.')
+
+  # Get the selected features
+  slct_intr <- out_intr %>% dplyr::slice(1) %>% dplyr::select(!!!rlang::syms(vars_intr)) %>%
+    dplyr::select_if(~. > 1) %>% unlist(., use.names = TRUE)
+
+  # Check if interactions improve the fit with only main effects
+  if (min(out_intr$cv_err) < min(out_main$cv_err)) slct_feat <- c(slct_main, slct_intr) else slct_feat <- slct_main
+
+  # Segment the data based on the selected features and optimal number of groups
+  data_segm <- maidrr::segmentation(fx_vars = c(fx_main, fx_intr)[names(slct_feat)], data = data, type = 'ngroups', values = slct_feat)
+
+  # Fit the optimal surrogate GLM
+  glm_par[['formula']] <- as.formula(paste(target, '~', paste(c(1, paste0(names(slct_feat), '_')), collapse = ' + ')))
+  opt_surro <- maidrr::surrogate(data = data_segm, par_list = glm_par)
+
+  # Combine results in a list and return
+  output <- list('slct_feat' = slct_feat,
+                 'best_surr' = opt_surro,
+                 'tune_main' = out_main,
+                 'tune_intr' = out_intr)
+  return(output)
 }
--- tests/testthat/test-autotune.R
+++ tests/testthat/test-autotune.R
@@ -17,15 +17,15 @@ gbm_fit <- gbm::gbm(as.formula(paste('nclaims ~',
                     interaction.depth = 3,
                     shrinkage = 0.1)
 gbm_fun <- function(object, newdata) mean(predict(object, newdata, n.trees = object$n.trees, type = 'response'))
-fx_vars <- gbm_fit %>% insights(vars = c('ageph', 'bm', 'coverage', 'fuel', 'bm_fuel', 'ageph_coverage'),
-                                data = mtpl_be,
-                                interactions = 'user',
-                                pred_fun = gbm_fun)
 
 
-test_that('output is of the expected format', {
-    lmbd_tune <- fx_vars %>% autotune(data = mtpl_be,
+
+test_that('output is of the expected format for only main effects', {
+    lmbd_tune <- gbm_fit %>% autotune(data = mtpl_be,
+                                      vars = c('ageph', 'bm', 'coverage', 'fuel', 'sex', 'fleet', 'use'),
                                       target = 'nclaims',
+                                      hcut = -1,
+                                      pred_fun = gbm_fun,
                                       lambdas = as.vector(outer(seq(1, 10, 1), 10^(-4:-2))),
                                       nfolds = 5,
                                       strat_vars = c('nclaims', 'expo'),
@@ -34,58 +34,82 @@ test_that('output is of the expected format', {
                                       err_fun = poi_dev,
                                       ncores = -1)
 
-    expect_is(lmbd_tune, 'tbl_df')
-    expect_equal(nrow(lmbd_tune), 7)
-    expect_equal(ncol(lmbd_tune), 8 + length(names(fx_vars)))
-    expect_true(all(c('lambda_main', 'lambda_intr', 'cv_err', as.character(1:5), names(fx_vars)) %in% names(lmbd_tune)))
-    expect_equal(sum(is.na(lmbd_tune)), 0)
+    expect_is(lmbd_tune, 'list')
+    expect_is(lmbd_tune$slct_feat, 'integer')
+    expect_is(lmbd_tune$best_surr, 'glm')
+    expect_is(lmbd_tune$tune_main, 'tbl_df')
+    expect_null(lmbd_tune$tune_intr)
+    expect_equal(sum(grepl('_', names(lmbd_tune$slct_feat))), 0)
+})
+
+
+test_that('output is of the expected format when including interactions', {
+  lmbd_tune <- gbm_fit %>% autotune(data = mtpl_be,
+                                    vars = c('ageph', 'bm', 'coverage', 'fuel', 'sex', 'fleet', 'use'),
+                                    target = 'nclaims',
+                                    hcut = 0.75,
+                                    pred_fun = gbm_fun,
+                                    lambdas = as.vector(outer(seq(1, 10, 1), 10^(-4:-2))),
+                                    nfolds = 5,
+                                    strat_vars = c('nclaims', 'expo'),
+                                    glm_par = alist(family = poisson(link = 'log'),
+                                                    offset = log(expo)),
+                                    err_fun = poi_dev,
+                                    ncores = -1)
+
+  expect_is(lmbd_tune, 'list')
+  expect_is(lmbd_tune$slct_feat, 'integer')
+  expect_is(lmbd_tune$best_surr, 'glm')
+  expect_is(lmbd_tune$tune_main, 'tbl_df')
+  expect_is(lmbd_tune$tune_intr, 'tbl_df')
 })
 
 test_that('all inputs are checked accordingly', {
-  expect_error(fx_vars %>% autotune(data = mtpl_be,
-                                    target = 'numclaims',
-                                    lambdas = NULL),
+  expect_error(gbm_fit %>% autotune(data = mtpl_be,
+                                    vars = c('ageph', 'bm_power'),
+                                    target = 'nclaims'),
+               'No underscores allowed in the variable names, these are interpreted as interactions in maidrr.')
+  expect_error(gbm_fit %>% autotune(data = mtpl_be,
+                                    vars = c('ageph', 'license'),
+                                    target = 'nclaims'),
+               'All the variables needs to be present in the data.')
+  expect_error(gbm_fit %>% autotune(data = mtpl_be,
+                                    vars = c('ageph', 'coverage'),
+                                    target = 'numclaims'),
                'The target variable needs to be present in the data.')
-  expect_error(fx_vars %>% autotune(data = mtpl_be,
+  expect_error(gbm_fit %>% autotune(data = mtpl_be,
+                                    vars = c('ageph', 'coverage'),
                                     target = 'nclaims',
-                                    lambdas = NULL,
+                                    hcut = 1.5),
+               'Invalid value specified for hcut, must equal -1 or lie within the range \\[0, 1\\].')
+  expect_error(gbm_fit %>% autotune(data = mtpl_be,
+                                    vars = c('ageph', 'coverage'),
+                                    target = 'nclaims',
                                     nfolds = 1),
                'At least two folds are needed to perform K-fold cross-validation.')
-  expect_error(fx_vars %>% autotune(data = mtpl_be,
+  expect_error(gbm_fit %>% autotune(data = mtpl_be,
+                                    vars = c('ageph', 'coverage'),
                                     target = 'nclaims',
-                                    lambdas = NULL,
                                     strat_vars = 'wrong_var'),
                'The stratification variables in strat_vars need to be present in the data.')
-  expect_error(fx_vars %>% autotune(data = mtpl_be,
+  expect_error(gbm_fit %>% autotune(data = mtpl_be,
+                                    vars = c('ageph', 'coverage'),
                                     target = 'nclaims',
-                                    lambdas = NULL,
                                     err_fun = function(y_pred) mean(y_pred)),
                'The error function must contain arguments y_true and y_pred.')
-  expect_error(fx_vars %>% autotune(data = mtpl_be,
+  expect_error(gbm_fit %>% autotune(data = mtpl_be,
+                                    vars = c('ageph', 'coverage'),
                                     target = 'nclaims',
-                                    lambdas = NULL,
                                     err_fun = function(y_pred, y_true, y_other) mean(y_pred)),
                'The error function can only contain arguments y_true, y_pred and w_case.')
-  expect_error(fx_vars %>% autotune(data = mtpl_be,
+  expect_error(gbm_fit %>% autotune(data = mtpl_be,
+                                    vars = c('ageph', 'coverage'),
                                     target = 'nclaims',
-                                    lambdas = NULL,
                                     err_fun = function(y_pred, y_true, w_case) mean(y_pred)),
                'If w_case is an argument in err_fun, weights must be an argument in glm_par.')
-  expect_error(fx_vars %>% autotune(data = mtpl_be,
+  expect_error(gbm_fit %>% autotune(data = mtpl_be,
+                                    vars = c('ageph', 'coverage'),
                                     target = 'nclaims',
-                                    lambdas = NULL,
                                     ncores = 0),
                'The number of cores must be strictly positive, or equal to -1 for all available cores.')
-  expect_warning(fx_vars %>% autotune(data = mtpl_be,
-                                      target = 'nclaims',
-                                      lambdas = as.vector(outer(seq(1, 10, 1), 10^(-4:-2))),
-                                      nfolds = 2,
-                                      ncores = 10),
-                 'The asked number of cores is larger than parallel::detectCores, so there might be trouble ahead.')
-  expect_warning(fx_vars %>% autotune(data = mtpl_be,
-                                      target = 'nclaims',
-                                      lambdas = as.vector(outer(seq(1, 10, 1), 10^(-4:-2))),
-                                      nfolds = 2,
-                                      glm_par = alist(formula = nclaims ~ 1)),
-                 'The field "formula" is removed from glm_par as this is determined by the supplied target and automatic feature selection.')
 })
